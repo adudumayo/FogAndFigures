36462d07eacef19c4d987ebe7619a6954efbacd0
Asimanye Dudumayo <adudumayo023@student.wethinkcode.co.za>
2024-08-11 20:25:25 +0200

    Setting up

---
+++ FogAndFigures/src/main/java/com/example/MainFigure.java
@@ -0,0 +1,16 @@
+public class MainFigure {
+    private String name;
+    //private Position position;
+
+    public void forward() {
+
+    }
+
+    public void back() {
+
+    }
+
+    public void cram() {
+
+    }
+}
