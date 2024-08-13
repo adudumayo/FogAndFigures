560bd21e65835439a7a8fbf24e8fe61557dd6bbb
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-08-13 12:40:03 +0200

    Paint the figure

--- FogAndFigures/src/main/java/com/example/Figure.java
+++ FogAndFigures/src/main/java/com/example/Figure.java
@@ -9,8 +9,20 @@ import java.awt.event.KeyEvent;
 
 public class Figure extends JPanel {
 
-    private int figureX = 0;
-    private int figureY = 0;
+    private int figureX = 10;
+    private int figureY = 10;
     private int figureWidth = 20;
     private int figureHeight = 20;
+
+    public Figure() {
+        // constructor
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.BLUE);
+        g.fillRect(figureX, figureY, figureWidth, figureHeight);
+    }
 }
