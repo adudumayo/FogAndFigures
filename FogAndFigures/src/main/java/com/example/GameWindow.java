4bcaf3938499ca15dafc179af176a154901ba118
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-08-13 11:43:17 +0200

    Add the code for the game window

---
+++ FogAndFigures/src/main/java/com/example/GameWindow.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import javax.swing.*;
+import java.awt.Dimension;
+
+public class GameWindow {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Fog and Figures");
+
+        window.setPreferredSize(new Dimension(800, 800));
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
